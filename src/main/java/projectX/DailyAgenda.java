3b90de7e5189d8b5cb8af7475f9f7e37440087ee
quisqo <quisqo@users.noreply.github.com>
2017-12-18 15:15:44 +0100

    Gaat om het ideely agenda

--- src/main/java/projectX/DailyAgenda.java
+++ src/main/java/projectX/DailyAgenda.java
@@ -1,5 +1,22 @@
 package projectX;
 
+import java.time.*;
+
 public class DailyAgenda extends AgendaItem {
+	
+	private String Task;
+		LocalTime beginTask;
+		LocalTime endTask;
+		
+	
+	public DailyAgenda() {
+		this.Task = "newTask";
+		
+	}
+
+		public void ProgressTask() {
+			//While loop????
+		}
+	     
 
 }
